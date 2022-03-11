a1a76fcbe57aabb62417761059343b3e7cb57d2b
deasar-js <deasartechnologies@gmail.com>
2022-03-11 16:21:59 +0000

    import bootstrap js and keep main dropdown on page

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -16,45 +16,57 @@ export default function NavigationBar() {
           </div>
 
           <div
-            className="navber-item dropdown ms-auto show k bm-0 mx-5"
+            className="navbar-item dropdown ms-auto show k bm-0 mx-5"
             aria-expanded="true"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="black"
-              className="bi bi-list"
-              viewBox="0 0 13 13"
+            <button
+              className="btn btn-primary "
+              type="button"
+              id="nav-dropdown"
+              data-bs-toggle="dropdown"
             >
-              <path
-                fillRule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              />
-            </svg>
-            <div className="dropdown-content">
-              <ul className="no-bullets">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="black"
+                className="bi bi-list"
+                viewBox="0 0 13 13"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                />
+              </svg>
+            </button>
+            <ul
+              className="dropdown-menu drop no-bullets p-2 shadow-sm mb-5 bg-body rounded"
+              aria-labelledby="nav-dropdown"
+            >
+              <li>
+                <Link
+                  id="sign"
+                  className="dropdown-menu-item no-styling"
+                  to="/user"
+                >
+                  {loggedInUser ? "Profile" : "Sign in"}
+                </Link>
+              </li>
+              {loggedInUser ? (
                 <li>
-                  <Link id="sign" className="no-styling" to="/user">
-                    {loggedInUser ? "Profile" : "Sign in"}
+                  <Link
+                    id="sign"
+                    className="dropdown-menu-item no-styling"
+                    to=""
+                    onClick={() => setLoggedInUser("")}
+                  >
+                    Sign out
                   </Link>
                 </li>
-                {loggedInUser ? (
-                  <li>
-                    <Link
-                      id="sign"
-                      className="no-styling"
-                      to=""
-                      onClick={() => setLoggedInUser("")}
-                    >
-                      Sign out
-                    </Link>
-                  </li>
-                ) : (
-                  ""
-                )}
-              </ul>
-            </div>
+              ) : (
+                ""
+              )}
+            </ul>
           </div>
         </nav>
       </header>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
 
 ReactDOM.render(
   <React.StrictMode>
